a59698c8920c3d724e20c51a8b03c05cc250ec73
Israel Abazie <101069893+vintage-creator@users.noreply.github.com>
2024-04-12 00:02:50 +0100

    Update page.tsx

--- src/app/book-appointment/page.tsx
+++ src/app/book-appointment/page.tsx
@@ -186,7 +186,7 @@ const BookAppointment = ({
           </div>
 
           <div className="w-full bg-blue100 p-[28px] mt-[48px] lg:mt-0 h-[350px] rounded-[10px]">
-            <p className={`text-[16px] text-center lg:text-[18px] mb-[20px]`}>
+            <p className={`text-[16px] text-center lg:text-[18px] mb-[30px]`}>
               Please pick a date for your service
             </p>
 
