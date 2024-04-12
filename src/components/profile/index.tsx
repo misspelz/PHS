b1a3d1d2b8e71b063632b0677d17c264949059f8
Israel Abazie <101069893+vintage-creator@users.noreply.github.com>
2024-04-12 13:11:44 +0100

    Update index.tsx

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -105,7 +105,7 @@ const Profile: React.FC<ProfileProps> = ({ phoneNumber = "" }) => {
                 value={formData.name}
                 onChange={handleChange}
                 placeholder="Name"
-                className="py-[16px] px-[24px] w-full border-inputBorder text-[12px] lg:text-[14px] outline-none"
+                className="py-[16px] px-[24px] w-full border-2 border-inputBorder text-[12px] lg:text-[14px] outline-none"
               />
             </div>
 
@@ -116,7 +116,7 @@ const Profile: React.FC<ProfileProps> = ({ phoneNumber = "" }) => {
                 value={formData.email}
                 onChange={handleChange}
                 placeholder="Email"
-                className="py-[16px] px-[24px] w-full border-inputBorder text-[12px] lg:text-[14px] outline-none"
+                className="py-[16px] px-[24px] w-full border-2 border-inputBorder text-[12px] lg:text-[14px] outline-none"
               />
             </div>
 
