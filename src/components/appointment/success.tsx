d430265b8b58a2d3d3fd65545c3555ca17ddec7f
Israel Abazie <101069893+vintage-creator@users.noreply.github.com>
2024-04-13 06:48:07 +0100

    Update success.tsx

--- src/components/appointment/success.tsx
+++ src/components/appointment/success.tsx
@@ -10,11 +10,11 @@ const Success: React.FC<SuccessProps> = ({ HandleClose }) => {
   return (
     <div className="px-6 mx-4 py-[30px] pb-[50px] lg:w-[600px]   bg-white rounded-[10px]">
       <div className="flex w-full justify-end">
-        <Image src={close} alt="close-icon" onClick={HandleClose} />
+        <Image src={close} alt="close-icon" onClick={HandleClose} className="cursor-pointer" />
       </div>
 
       <div className="flex flex-col items-center justify-center h-full max-w-[600px] w-full mx-auto">
-        <Image src={succesimg} alt="close-icon" className="" />
+        <Image src={succesimg} alt="success image" className="" />
         <p className="text-[18px] lg:text-[28px]  font-[600] mt-[24px] text-center">
           Congratulations
         </p>
