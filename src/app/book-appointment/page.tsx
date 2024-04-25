5d63b8e7626ff04ffdcffdd77fef62d249ff7ddc
Israel Abazie <101069893+vintage-creator@users.noreply.github.com>
2024-04-25 21:48:17 +0100

    Update page.tsx

--- src/app/book-appointment/page.tsx
+++ src/app/book-appointment/page.tsx
@@ -214,7 +214,7 @@ const BookAppointment = () => {
             </p>
 
             <div className="mt-[28px] flex items-center w-full justify-center scale-125">
-              <Calendar value={value} onChange={handleDateChange} disabled={isDateDisabled(value)} />
+              <Calendar value={value} onChange={handleDateChange} className={isDateDisabled(value) ? "opacity-50 cursor-not-allowed" : ""} />
             </div>
           </div>
         </div>
