ac42e65cf190fc2ea4a638419a046bb4df8bca89
Israel Abazie <101069893+vintage-creator@users.noreply.github.com>
2024-04-26 20:34:35 +0100

    Update page.tsx

--- src/app/book-appointment/page.tsx
+++ src/app/book-appointment/page.tsx
@@ -234,7 +234,7 @@ const BookAppointment = () => {
  <div className="mt-[35px] flex items-center justify-center">
   <div className="flex items-center mr-4">
     <div 
-      className="w-4 h-4 rounded-full bg-blue-500 mr-2" 
+      className="w-4 h-4 rounded-full bg-blue-300 mr-2" 
     ></div>
     Active Dates
   </div>
