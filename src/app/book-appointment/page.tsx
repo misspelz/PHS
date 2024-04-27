00c1ed1079157fe184916d012c1d1e4b767facfe
Israel Abazie <101069893+vintage-creator@users.noreply.github.com>
2024-04-27 07:28:19 +0100

    Update page.tsx

--- src/app/book-appointment/page.tsx
+++ src/app/book-appointment/page.tsx
@@ -107,7 +107,7 @@ const BookAppointment = () => {
     hours = 0; 
   }
    
-  const selectedTime = new Date();
+  const selectedTime = new Date(selectedDate.getTime());
   selectedTime.setHours(hours, minutes, 0, 0);
   const isPastTime = selectedDate < currentTime || (selectedDate.getDate() === currentTime.getDate() && selectedTime < currentTime);
   const isBookedTime = bookedTimes.some(bookedTime => bookedTime.time === time && bookedTime.date === formatDate(value));
